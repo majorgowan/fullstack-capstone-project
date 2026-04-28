475a6899b79674e6aaec04c6377c409db17df320
Mark Fruman <majorgowan@yahoo.com>
2026-04-28 13:11:18 -0400

    implemented backend in registration component

--- giftlink-frontend/src/components/DetailsPage/DetailsPage.js
+++ giftlink-frontend/src/components/DetailsPage/DetailsPage.js
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from 'react';
 import { useParams, useNavigate } from 'react-router-dom';
 import './DetailsPage.css';
+import {urlConfig} from '../../config';
+
 
 function DetailsPage() {
     const navigate = useNavigate();
--- giftlink-frontend/src/components/RegisterPage/RegisterPage.js
+++ giftlink-frontend/src/components/RegisterPage/RegisterPage.js
@@ -2,6 +2,11 @@ import React, {useState} from 'react';
 
 import './RegisterPage.css';
 
+import {urlConfig} from '../../config';
+import { useAppContext } from '../../context/AuthContext';
+import { useNavigate } from 'react-router-dom';
+
+
 function RegisterPage() {
 
     // insert code here to create useState hook variables for firstName, lastName, email, password
@@ -10,9 +15,51 @@ function RegisterPage() {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
 
+    const [showerr, setShowerr] = useState('');
+
+    const navigate = useNavigate();
+    const { setIsLoggedIn } = useAppContext();
+
     // insert code here to create handleRegister function and include console.log
     const handleRegister = async () => {
         console.log("Register invoked");
+
+        const response = await fetch(`${urlConfig.backendUrl}/api/auth/register`, {
+            //Step 1 - Task 6
+            method: 'POST',
+            //Step 1 - Task 7
+            headers: {
+                'content-type': 'application/json',
+            },
+            //Step 1 - Task 8
+            body: JSON.stringify({
+                firstName: firstName,
+                lastName: lastName,
+                email: email,
+                password: password
+            })
+        });
+
+        //Step 2 - Task 1
+        const json = await response.json();
+        console.log('json data', json);
+        console.log('er', json.error);
+
+        //Step 2 - Task 2
+        if (json.authtoken) {
+            sessionStorage.setItem('auth-token', json.authtoken);
+            sessionStorage.setItem('name', firstName);
+            sessionStorage.setItem('email', json.email);
+            //Step 2 - Task 3
+            setIsLoggedIn(true);
+            //Step 2 - Task 4
+            navigate('/app');
+        }
+        if (json.error) {
+            //Step 2 - Task 5
+            setShowerr(json.error);
+        }
+
     }
 
     return (
